166e5635552f9cdc67d3c2941450a899c8c6eaf2
Sean Febo <seanfebo@techie.com>
2018-03-04 09:50:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,4 @@ function logShout(string) {
 function logWhisper(STRING){
   console.log(STRING.toLowerCase())
 }
-function sayHiToGrandma(string) {
-  if (string === uppercase) {
-    return "YES INDEED!"}
-  elseif (string === "I love you, Grandma."); {
-    return "I love you, too."}
-    else return "I can\'t hear you!"
-}
+
